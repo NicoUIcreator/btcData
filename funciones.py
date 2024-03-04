cbe5c79626ae29e1349f2319283c6629bcacc42d
Nicolas Coronel <nicolasjuancoronel@gmail.com>
2024-03-04 14:27:02 +0100

    primer rep

---
+++ funciones.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+
+def json_to_dataframe(data_entrada):
+    elements = data_entrada['elements']
+    places = {'lat': [], 'lon': [], 'name': [], 'address': []}
+
+    for i in elements:
+        lalitude = i['lat']
+        longitude = i['lon']
+        name = i['tags'].get('name')
+        street = i['tags'].get('addr:street', 'Sin calle')
+        number = i['tags'].get('addr:housenumber', 0)
+        
+        places['lat'].append(lalitude)
+        places['lon'].append(longitude)
+        places['name'].append(name)
+        places['address'].append(str(street) + ' ' + str(number))
+
+    df = pd.DataFrame(places)
+    return df
