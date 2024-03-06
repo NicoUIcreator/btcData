ec1d669580206ac7c1e4d00363edb6651ac9e69c
Nicolas Coronel <nicolasjuancoronel@gmail.com>
2024-03-06 22:25:41 +0100

    060324avance

---
+++ utils/funciones.py
@@ -0,0 +1,1 @@
+str.replace(to_replace=(",","%"),value="",regex=True).replace(to_replace="M",value="0000",regex=True).replace(to_replace="K",value="0",regex=True).replace(to_replace="B",value="0000000",regex=True).astype(float)
