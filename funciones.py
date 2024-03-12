018a07c34678e11819ef0d0f2c1420e1b5b2b5ce
Nicolas Coronel <nicolasjuancoronel@gmail.com>
2024-03-12 18:16:54 +0100

    120324a2dias

--- funciones.py
+++ funciones.py
@@ -1,27 +1,4 @@
 
-import pandas as pd
-
-
-def json_to_dataframe(data_entrada):
-    elements = data_entrada['elements']
-    places = {'lat': [], 'lon': [], 'name': [], 'address': []}
-
-    for i in elements:
-        lalitude = i['lat']
-        longitude = i['lon']
-        name = i['tags'].get('name')
-        street = i['tags'].get('addr:street', 'Sin calle')
-        number = i['tags'].get('addr:housenumber', 0)
-        
-        places['lat'].append(lalitude)
-        places['lon'].append(longitude)
-        places['name'].append(name)
-        places['address'].append(str(street) + ' ' + str(number))
-
-    df = pd.DataFrame(places)
+def limpiezaBtcVsc(df):
+    df[["Price","Open","High","Low","Vol.","Change %"]].replace(to_replace=(",","%"),value="",regex=True).replace(to_replace="M",value="0000",regex=True).replace(to_replace="K",value="0",regex=True).replace(to_replace="B",value="0000000",regex=True).astype(float)
     return df
-
-def correlacion(df1,df2):
-
-    
-    return df1.corrwith(df2)
